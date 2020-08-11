f5d80d8a630c03c986c9a4631b31b87c018bec37
Michael Guay <mguay@MacBook-Pro.local>
2020-08-11 19:55:16 -0400

    Add env port

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const socketio = require('socket.io');
 
 app.use(express.static(__dirname + '/public'));
 
-const expressServer = app.listen(9000);
+const expressServer = app.listen(process.env.PORT || 9000);
 const io = socketio(expressServer);
 
 io.on('connection', (socket) => {
